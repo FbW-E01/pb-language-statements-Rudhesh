0c67110875707e7a92018d7aa2cedbc366ae54f0
dci <rudeshdhote@gmail.com>
2021-03-30 10:09:40 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,12 @@
+console.log"Hello World";
+
+// console.log("This is my first js file");
+/*
+Remove a parenthesis from the first statement and run the code. Does your statement print?
+
+No
+
+Is there an error? If so, what kind of error is it?
+
+SyntaxError: Unexpected string
+*/
